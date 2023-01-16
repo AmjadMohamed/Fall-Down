262526b122bf94e663556694382a83721c93ee83
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-01-16 13:53:49 +0200

    Implemented The Player Movement
    Used New Input System To Implement Movement

---
+++ Assets/Scripts/Amjad/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //public
+    [SerializeField] float speed;
+
+
+
+    // private
+    Rigidbody2D rb2d;
+    Vector2 moveInputVal;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    // this method gets the move input from the OnMove Action
+    private void OnMove(InputValue val)
+    {
+        moveInputVal = val.Get<Vector2>();
+        print(moveInputVal.x);
+    }
+
+    private void Movement()
+    {
+        rb2d.velocity = new Vector2(moveInputVal.x * speed * Time.fixedDeltaTime, rb2d.velocity.y);
+    }
+
+
+
+
+}
