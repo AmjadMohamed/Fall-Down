4870976844ee109042b459b8550ed9ca358ef87d
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-01-18 12:21:02 +0200

    Improved Sprites Quality And Collider Fix

--- Assets/Scripts/Amjad/TargetDoorInteraction.cs
+++ Assets/Scripts/Amjad/TargetDoorInteraction.cs
@@ -20,6 +20,7 @@ public class TargetDoorInteraction : MonoBehaviour
         {
             ArrivedPlayers++;
             collision.GetComponent<PlayerController>().enabled = false;
+            collision.transform.tag = "Untagged";
             Results.text += $"{ArrivedPlayers}- {collision.GetComponent<SpriteRenderer>().sprite.name} -> {TimeCounter.text}\n";
         }
     }
