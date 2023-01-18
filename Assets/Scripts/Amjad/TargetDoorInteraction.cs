4ecf2936011e709667aa7cf0ea4883757466785f
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-01-18 08:45:50 +0200

    Added The Ability To Restart Game

--- Assets/Scripts/Amjad/TargetDoorInteraction.cs
+++ Assets/Scripts/Amjad/TargetDoorInteraction.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TargetDoorInteraction : MonoBehaviour
 {
     [SerializeField] JoinedPlayersController joinedPlayersController;
     [SerializeField] GameObject WinGamePanel;
+    [SerializeField] Timer timer;
+    [SerializeField] TextMeshProUGUI TimeCounter;
+    [SerializeField] TextMeshProUGUI Results;
     private int ArrivedPlayers;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -14,6 +18,7 @@ public class TargetDoorInteraction : MonoBehaviour
         {
             ArrivedPlayers++;
             collision.GetComponent<PlayerController>().enabled = false;
+            Results.text += $"{ArrivedPlayers}- {collision.GetComponent<SpriteRenderer>().sprite.name} -> {TimeCounter.text}\n";
         }
     }
 
@@ -22,6 +27,7 @@ public class TargetDoorInteraction : MonoBehaviour
         if (ArrivedPlayers == joinedPlayersController.PlayersCount && ArrivedPlayers > 0)
         {
             WinGamePanel.SetActive(true);
+            timer.timeIsRunnig = false;
         }
     }
 }
--- Assets/Scripts/Serag/Timer.cs
+++ Assets/Scripts/Serag/Timer.cs
@@ -17,21 +17,21 @@ public class Timer : MonoBehaviour
 
     private void Update()
     {
-        if(timeIsRunnig)
+        if (timeIsRunnig)
         {
             PressAnyButtonTextGO.SetActive(false);
-            if(timeRemaining >=0)
+            if (timeRemaining >= 0)
             {
                 timeRemaining += Time.deltaTime;
-                DisplayTime(timeRemaining);
+                timeText.text = DisplayTime(timeRemaining);
             }
         }
     }
-    void DisplayTime(float timeToDisplay)
+    public string DisplayTime(float timeToDisplay)
     {
         timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timeText.text = string.Format("{00:00} : {1:00}", minutes, seconds);
+        return string.Format("{00:00} : {1:00}", minutes, seconds);
     }
 }
